e8706155d76dd64caccf8f58be271cbe68f17d32
Simon Kinslow <simonkinslow@gmail.com>
2014-07-09 13:42:08 +0100

    Pixel Test
    Sprite and tree update tests (Non-Retina _1x only)

--- _assets/_js/Battle.js
+++ _assets/_js/Battle.js
@@ -1167,7 +1167,7 @@
 	{
 		// BATTLE_NAV.game.result = battleEngine.battle(MAP_PLAYER, ROM.enemy.character, false);
 		
-		BATTLE_NAV.game.result = "DRAW";
+		BATTLE_NAV.game.result = "LOSE";
 			
 		battleNav_logicDisplay();
 	}
