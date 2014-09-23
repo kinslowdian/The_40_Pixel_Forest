128b26982eaf162d8841bed51151194d31a5a423
Simon Kinslow <skinslow@tmw.co.uk>
2014-09-23 18:24:48 +0100

    New Keyboard Hint
    Keyboard hint with updated design, code clean up taking last parts out of the temp.css file and merging it into game.css and pixels.css

--- _assets/_js/Game_Control.js
+++ _assets/_js/Game_Control.js
@@ -37,6 +37,9 @@
 
 	var game_introEntrance = true;
 
+	/* --- KEYBOARD HINT */
+
+	var keyboardHint;
 
 	function mapPlayer_init(playerMover, playerTween, playerWalkTweenX, playerWalkTweenY, playerWalkStop, playerWalkLoop, playerFadeTarget, hitTestArea)
 	{
@@ -1309,6 +1312,92 @@
 	}
 
 	///////////////////////////////// --- HITTEST
+
+
+
+
+	///////////////////////////////// --- KEYBOARD HINT
+
+	function keyboardHint_init(use)
+	{
+		if(use)
+		{
+			keyboardHint = {};
+
+			keyboardHint.firstInit = true;
+			keyboardHint.delay_show = null;
+			keyboardHint.delay_hide = null;
+			keyboardHint.inView = false;
+			keyboardHint.waitingForPress = false;
+
+			keyboardHint.randomPress = new Array();
+			keyboardHint.randomPress = ["keyboardHint_pressU", "keyboardHint_pressD", "keyboardHint_pressL", "keyboardHint_pressR"];
+		}
+
+		else
+		{
+			$(".keyboardHint_wrapper").remove();
+		}
+	}
+
+	function keyboardHint_run()
+	{
+		if(!keyboardHint.waitingForPress)
+		{
+			keyboardHint.waitingForPress = true;
+
+			$(".keyboardHint_pressBtn").addClass(keyboardHint.randomPress[Math.floor(Math.random() * keyboardHint.randomPress.length)]);
+
+			keyboardHint.delay_show = setTimeout(keyboardHint_switch, 2 * 1000, true);
+		}
+	}
+
+	function keyboardHint_cancel()
+	{
+		clearTimeout(keyboardHint.delay_show);
+		clearTimeout(keyboardHint.delay_hide);
+
+		if(keyboardHint.inView)
+		{
+			keyboardHint_switch(false);
+		}
+	}
+
+	function keyboardHint_switch(use)
+	{
+		if(use && !keyboardHint.inView)
+		{
+			keyboardHint.inView = true;
+
+			$(".keyboardHint_wrapper").css("opacity", "1");
+
+			keyboardHint.delay_hide = setTimeout(keyboardHint_switch, 10 * 1000, false);
+		}
+
+		else
+		{
+			keyboardHint.inView = false;
+
+			keyboardHint.waitingForPress = false;
+
+			$(".keyboardHint_wrapper").css("opacity", "0");
+
+			$(".keyboardHint_wrapper")[0].addEventListener("webkitTransitionEnd", keyboardHint_purge, false);
+			$(".keyboardHint_wrapper")[0].addEventListener("transitionEnd", keyboardHint_purge, false);
+		}
+	}
+
+	function keyboardHint_purge(event)
+	{
+		$(".keyboardHint_wrapper")[0].removeEventListener("webkitTransitionEnd", keyboardHint_purge, false);
+		$(".keyboardHint_wrapper")[0].removeEventListener("transitionEnd", keyboardHint_purge, false);
+
+		$(".keyboardHint_wrapper").remove();
+
+		delete keyboardHint;
+	}
+
+	///////////////////////////////// --- KEYBOARD HINT
 
 
 
